4228b189c2898668da8d9525c2ceef577779f20b
Humza Khan <83923770+HumzaKhan071@users.noreply.github.com>
2021-12-02 02:20:28 +0500

    update

--- lib/pages/detail/detail.dart
+++ lib/pages/detail/detail.dart
@@ -1,4 +1,6 @@
+import 'package:ebook_app/constants/colors.dart';
 import 'package:ebook_app/models/book.dart';
+import 'package:ebook_app/pages/detail/widgets/book_detail.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
@@ -8,7 +10,31 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
+      backgroundColor: Colors.white,
+      appBar: _buildAppBar(context),
+      body:SingleChildScrollView(
+        child: Column(
+          children: [
+            BookDetail(book:book),
+          ],
+        ),
+      ) ,
     );
   }
+
+ AppBar _buildAppBar(BuildContext context) {
+
+   return AppBar(
+     backgroundColor: Colors.white,
+     elevation:0,
+
+     leading: IconButton(onPressed: (){
+       Navigator.pop(context);
+     },icon: Icon(Icons.arrow_back_ios_outlined),color: kFont,),
+     actions: [
+       IconButton(onPressed: (){}, icon: Icon(Icons.more_horiz_outlined),color: kFont,)
+     ],
+
+   );
+ }
 }
---
+++ lib/pages/detail/widgets/book_detail.dart
@@ -0,0 +1,12 @@
+import 'package:ebook_app/models/book.dart';
+import 'package:flutter/material.dart';
+
+class BookDetail extends StatelessWidget {
+  final Book book;
+  const BookDetail({Key? key, required this.book}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
