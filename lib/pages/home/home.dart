fa5e2722af849d52925b52944be670b76fdda7ce
Humza Khan <83923770+HumzaKhan071@users.noreply.github.com>
2021-12-03 00:53:09 +0500

    update

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ebook_ui/constants/colors.dart';
+import 'package:flutter_ebook_ui/pages/home/widgets/custom_tab.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,17 +10,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var tabIndex = 0;
+  final pageController = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: _buildAppBAr(),
+      body: Column(
+        children: [
+          CustomTab(
+              selectdindex: tabIndex,
+              callback: (int index) {
+                setState(() {
+                  tabIndex = index;
+                });
+
+                pageController.jumpToPage(index);
+              })
+        ],
+      ),
     );
   }
 
   AppBar _buildAppBAr() {
     return AppBar(
-      backgroundColor:Colors.white,
-      
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.menu_rounded,
+            color: kFont,
+          )),
+      title: Text(
+        "All Books",
+        style: TextStyle(color: kFont, fontSize: 18),
+      ),
+      centerTitle: true,
+      actions: [
+        IconButton(
+            onPressed: () {}, icon: Icon(Icons.search_outlined, color: kFont))
+      ],
     );
   }
 }
---
+++ lib/pages/home/widgets/custom_tab.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CustomTab extends StatelessWidget {
+  final int selectdindex;
+  final Function callback;
+
+  CustomTab({Key? key, required this.selectdindex, required this.callback})
+      : super(key: key);
+
+  final tabs = ["Ebooks", "Audio Books"];
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Container(
+      width: width,
+      height: 56,
+      margin: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: ListView.separated(
+          padding: EdgeInsets.symmetric(horizontal: 5),
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (_, index) => GestureDetector(
+                onTap: () => callback(index),
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 5),
+                  alignment: Alignment.center,
+                  width: (width - 40) / 2 - 10,
+                  decoration: BoxDecoration(
+                    color:
+                        selectdindex == index ? Colors.white : Colors.grey[200],
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(
+                    tabs[index],
+                    style: TextStyle(),
+                  ),
+                ),
+              ),
+          separatorBuilder: (_, index) => SizedBox(
+                width: 10,
+              ),
+          itemCount: tabs.length),
+    );
+  }
+}
